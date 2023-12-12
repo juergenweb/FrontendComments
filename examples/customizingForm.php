d3bb9ddeac3d7f7c0f7b9e0f9a7962a980349b4b
juergenweb <juergenweb@users.noreply.github.com>
2023-12-12 14:00:56 +0100

    Update customizingForm.php

--- examples/customizingForm.php
+++ examples/customizingForm.php
@@ -1,17 +1,17 @@
 <?php
-declare(strict_types=1);
+    declare(strict_types=1);
 
-/*
- * Customizing the form and its fields
- *
- * Created by Jürgen K.
- * https://github.com/juergenweb 
- * File name: customizingForm.php
- * Created: 23.07.2023 
- */
+    /*
+     * Customizing the form and its fields
+     *
+     * Created by Jürgen K.
+     * https://github.com/juergenweb
+     * File name: customizingForm.php
+     * Created: 23.07.2023
+     */
 
-$comments = $page->nameOfYourCommentField;
-$form = $comments->getCommentForm();// get the form object
-$form->getFormElementByName('email')->setLabel('My custom email label'); // grab the email field and
-echo $form->render();
+    $comments = $page->nameOfYourCommentField;
+    $form = $comments->getCommentForm();// get the form object
+    $form->getFormElementByName('email')->setLabel('My custom email label'); // grab the email field and
+    echo $form->render();
 
